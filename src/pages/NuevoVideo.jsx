8d7e6244648a61d1b4e7426205599b4006e2c177
Emanuel <emanuel.cortezm@gmail.com~>
2024-07-14 19:48:49 -0600

    ca

--- src/pages/NuevoVideo.jsx
+++ src/pages/NuevoVideo.jsx
@@ -14,6 +14,7 @@ const Container = styled.div`
   justify-content: baseline;
   width: 100%;
   @media (min-width: 320px) and (max-width: 480px) {
+    height: 5px;
     width :480px ;
     font-size: 1.5em;
     }
